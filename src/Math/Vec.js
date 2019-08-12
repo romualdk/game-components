d9030554a04c00c1ede32d8436335ec8c9438353
romualdk <romuald.kowalczyk@gmail.com>
2019-08-12 22:11:49 +0200

    cleanup

--- src/Math/Vec.js
+++ src/Math/Vec.js
@@ -4,12 +4,12 @@
  * https://github.com/xem/mini2Dphysics/blob/gh-pages/index.html
  */
 
-export const Vec2 = (x, y) => ({ x, y })
-export const add = (a, b) => Vec2(a.x + b.x, a.y + b.y)
+export const Vec = (x, y) => ({ x, y })
+export const add = (a, b) => Vec(a.x + b.x, a.y + b.y)
 export const subtract = (a, b) => add(a, scale(b, -1))
-export const multiply = (a, b) => Vec2(a.x * b.x, a.y * b.y)
-export const divide = (a, b) => Vec2(a.x / b.x, a.y / b.y)
-export const scale = (a, scalar) => Vec2(a.x * scalar, a.y * scalar)
+export const multiply = (a, b) => Vec(a.x * b.x, a.y * b.y)
+export const divide = (a, b) => Vec(a.x / b.x, a.y / b.y)
+export const scale = (a, scalar) => Vec(a.x * scalar, a.y * scalar)
 export const length = a => dot(a, a) ** 0.5
 export const distance = (a, b) => length(subtract(a, b))
 export const dot = (a, b) => a.x * b.x + a.y * b.y
@@ -17,5 +17,8 @@ export const cross = (a, b) => a.x * b.y - a.y * b.x
 export const normalize = a => scale(a, 1 / (length(a) || 1))
 export const project = (a, b) => scale(b, dot(a, b) / dot(b, b))
 export const angle = (a) => Math.atan2(a.y, a.x)
-export const rotate = (a, angle, g = Vec2(Math.sin(angle), Math.cos(angle))) =>
-  Vec2(cross(a, g), dot(a, g))
+export const rotate = (a, angle, g = Vec(Math.sin(angle), Math.cos(angle))) =>
+  Vec(cross(a, g), dot(a, g))
+export const angle2 = (a, b) => angle(subtract(b, a))
+export const rotate2 = (a, origin, angle) =>
+  add(origin, rotate(subtract(a, origin), angle))
